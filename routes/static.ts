29922b4a48e296021933a119b69479e0a1cb71ee
ejaz4 <ejazali4@protonmail.ch>
2022-12-04 21:11:00 +0000

    ✨ JSON support from main route

--- routes/static.ts
+++ routes/static.ts
@@ -8,6 +8,7 @@ import { search } from '../search';
 type SearchRequest = FastifyRequest<{
     Querystring: {
         q: string;
+        format?: string;
     }
 }>
 
@@ -17,23 +18,29 @@ export const staticRoutes = () => {
     const indexFile = fs.readFileSync("./routes/static/index.html").toString();
 
     server.register(fastifyStatic, {
-        root: path.join(__dirname, 'static/css'),
-        prefix: '/css',
-        decorateReply: true
+        root: path.join(__dirname, 'static'),
+        prefix: '/',
     })
 
     server.get("/search", async(request: SearchRequest, reply) => {
         var searchReply = searchFile.replaceAll("<query>", request.query.q);
         var results = await search(request.query.q, "web");
         var resultsElem = "";
+        var format = request.query.format ? request.query.format : "html";
 
-        for (const item of results) {
-            resultsElem += `<div class="result"><a href="${item.link.href}"><h3>${item.title}</h3></a><p>${item.description}</p></div>`
+        if (format == "html") {
+            for (const item of results) {
+                resultsElem += `<div class="result"><a href="${item.link.href}"><h3 class="result-title">${item.title}</h3></a><p class="result-link">${item.link.href}</p><p class="result-description">${item.description}</p></div>`
+            }
+
+            searchReply = searchReply.replaceAll("<results>", resultsElem);
+
+            reply.type("text/html").send(searchReply);
         }
 
-        searchReply = searchReply.replaceAll("<results>", resultsElem);
-
-        reply.type("text/html").send(searchReply);
+        if (format == "json") {
+            reply.type("application/json").send(results);
+        }
     });
 
     server.get("/", (request, reply) => {
